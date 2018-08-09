04f9dab0808edae8be8390b20f6052db48a6e6a5
Steveland Daniels <steveland@backbase.com>
2018-08-09 22:16:34 +0100

    Further work on setting up the game and dealing out the cards to players

--- src/main/java/com/project/starwars/toptrumpsMVC/ToptrumpsMvcApplication.java
+++ src/main/java/com/project/starwars/toptrumpsMVC/ToptrumpsMvcApplication.java
@@ -1,8 +1,9 @@
 package com.project.starwars.toptrumpsMVC;
 
-import com.project.starwars.toptrumpsMVC.controller.TopTrumpGameController;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
+import static com.project.starwars.toptrumpsMVC.controller.TopTrumpGameController.startGame;
 
 //@SpringBootApplication
 public class ToptrumpsMvcApplication {
@@ -10,9 +11,9 @@ public class ToptrumpsMvcApplication {
 	/*public static void main(String[] args) {
 		SpringApplication.run(ToptrumpsMvcApplication.class, args);
 	*/
-    public static void main (String args[]) {
+    public static void main (String args[]) throws IOException {
 
-        //Call the method to start the game
-        TopTrumpGameController.start();
+        startGame();
+
     }
 }
--- src/main/java/com/project/starwars/toptrumpsMVC/controller/TopTrumpGameController.java
+++ src/main/java/com/project/starwars/toptrumpsMVC/controller/TopTrumpGameController.java
@@ -1,59 +1,75 @@
 package com.project.starwars.toptrumpsMVC.controller;
-
-
 import com.project.starwars.toptrumpsMVC.model.Card;
 import com.project.starwars.toptrumpsMVC.model.Deck;
 import com.project.starwars.toptrumpsMVC.model.Player;
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 
 
 import java.io.IOException;
+import java.util.Scanner;
 import java.util.Stack;
+
 
 public class TopTrumpGameController {
 
     //Reset Deck, Hands, Score and Deal Cards
-
     /**
      * Starts the initial Top Trump
      * Resets the deck
      * Initialises the players
      */
-    public static void start() throws IOException {
+    private static void playerSetup(){
 
-        String nameofPlayer;
+        //  Set up the Players
 
         System.out.println("Welcome to the Star Wars Top Trumps");
 
+        Scanner input = new Scanner(System.in);
+        System.out.print("Player One - Enter your name: /n");
+        String nameofPlayer = input.nextLine();
 
-        private static void playerSetup() {
-
-
-
-            //  Set up the Players
-
-            Player playerOne = new Player(nameofPlayer);
-            Player playerTwo = new Player("Computer");
+        Player playerOne = new Player(nameofPlayer);
+        Player playerTwo = new Player("Computer");
         }
 
-        private void deckSetUp()
-        {
-            //Set up the Deck
-            JsontoPojoConvert c = new JsontoPojoConvert();
-            Deck deck = new Deck(c.readCardsFromJson());
-        }
+    private static void deckSetup() throws IOException {
 
+        //Set up the Deck
+        JsontoPojoConvert c = new JsontoPojoConvert();
+        Deck deck = new Deck(c.readCardsFromJson());
 
-        //Deal to Players
+    }
+
+    private static void dealToPlayers() throws IOException {
+
+        Stack<Card> playerHand1 = new Stack<>();
+        Stack<Card> playerHand2 = new Stack<>();
+
         int x = 0;
         int y = 0;
 
         while (deck.getSizeofDeck() != 0) {
 
 
-            player1.add(deck.dealSingleCard());
-            player2.add(deck.dealSingleCard());
+            playerHand1.add(deck.dealSingleCard());
+            playerHand2.add(deck.dealSingleCard());
 
-            deck.shuffle();
+            System.out.println("Player 1: " + playerHand1.get(x).getName());
+            System.out.println("Computer Player: " + playerHand2.get(y).getName());
+            System.out.println("");
+
+            x++;
+            y++;
+        }
 
     }
+
+    public static void startGame() throws IOException {
+
+        playerSetup();
+        deckSetup();
+    }
+
+
+
 }
